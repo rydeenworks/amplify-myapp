c6a79c044e5da34afa8c1923a3f6495170fbc94e
RydeenWorks <rydeenworks@gmail.com>
2024-09-12 11:18:23 +0900

    フロントエンド側

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Authenticator } from '@aws-amplify/ui-react'
+import { InvokeCommand, LambdaClient } from '@aws-sdk/client-lambda'
+import { fetchAuthSession } from 'aws-amplify/auth'
+
+import outputs from "../amplify_outputs.json"
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+  async function invokeHelloWorld() {
+
+    const { credentials } = await fetchAuthSession()
+    const awsRegion = outputs.auth.aws_region
+    const functionName = outputs.custom.helloWorldFunctionName
+
+    const labmda = new LambdaClient({ credentials: credentials, region: awsRegion })
+    const command = new InvokeCommand({
+      FunctionName: functionName,
+    });
+    const apiResponse = await labmda.send(command);
+
+    if (apiResponse.Payload) {
+      const payload = JSON.parse(new TextDecoder().decode(apiResponse.Payload))
+      setText(payload.message)
+    }
+  }
 
   return (
     <Authenticator>
@@ -31,6 +55,10 @@ function App() {
           </div>
           <p className="read-the-docs">
             Click on the Vite and React logos to learn more
+          </p>
+          <p>
+            <button onClick={invokeHelloWorld}>invokeHelloWorld</button>
+            <div>{text}</div>
           </p>
         </>
       )}
