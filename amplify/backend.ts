6fc91adb6f63a5b2982b55cae37eee49399ff9c9
RydeenWorks <rydeenworks@gmail.com>
2024-09-12 10:52:01 +0900

    Lambdaを呼び出すための権限を付与

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,10 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource'
 import { helloWorld } from './function/hello-world/resource'
 
-defineBackend({
+const backend = defineBackend({
   auth,
   helloWorld,
 });
+
+const authenticatedUserIamRole = backend.auth.resources.authenticatedUserIamRole;
+backend.helloWorld.resources.lambda.grantInvoke(authenticatedUserIamRole);
